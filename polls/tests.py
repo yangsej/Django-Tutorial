952e6c181a2310bc974c123700c814d36c034c89
양세준 <yangsej@naver.com>
2016-05-28 12:39:57 +0900

    2016-05-28 12:39
    장고 튜토리얼 5

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,16 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """was_published_recently() 는 pub_date가 미래인 질문에 대해서
+Fasle를 반환해야 합니다."""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+
+        self.assertEqual(future_question.was_published_recently(), False)
--- polls/views.py
+++ polls/views.py
@@ -2,32 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.template import loader
+from django.views import generic
 from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list,}
-    return HttpResponse(template.render(context,request))
-##    output = ', '.join([q.question_text for q in latest_question_list])
-##    return HttpResponse(output)
+class IndexView(generiv.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'last_question_list'
+    
+    def get_queryset(self):
+    """최신 질문 5개를 반환합니다."""
+    return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-##    try:
-##        question = Question.objects.get(pk=question_id)
-##    except Question.DoesNotExist:
-##        raise Http404("존재하지 않는 질문입니다.")
-##    return render(request, "polls/detail.html",{'question': question})
-##    return HttpResponse("당신은 질문 %s을 보고 있습니다."% question_id)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-##    response = "당신은 질문 %s의 결과를 보고 있습니다."
-##    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -43,5 +38,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-##    return HttpResponse("당신은 질문 %s에 투표하고 있습니다."% question_id)
-
